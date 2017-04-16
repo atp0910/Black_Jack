3256e879b9d11870671f32db7bb9424f772bee46
atp0910 <ap0910@gmail.com>
2017-04-16 15:45:02 -0400

    Split and double down functions working.

---
+++ app/src/test/java/com/example/atp0910/black_jack/BlackJackCasinoTest.java
@@ -0,0 +1,16 @@
+package com.example.atp0910.black_jack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by atp0910 on 4/15/17.
+ */
+public class BlackJackCasinoTest {
+    @Test
+    public void checkWinner() throws Exception {
+
+    }
+
+}
